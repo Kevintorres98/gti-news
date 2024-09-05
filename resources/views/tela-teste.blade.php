71e3f2f6302beabb4fecfe46d2a4cd0ff7e28885
Kevintorres98 <torres98k@gmail.com>
2024-09-05 10:37:54 -0300

    criacao do projeto, tailwind , e tabelas

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>tela de teste</title>
+</head>
+<body>
+<h1 class= "bg-black p-4 rounded-lg text-white">Hello Mundo</h1>    
+</body>
+</html>
